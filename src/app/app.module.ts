7e84e1791c43a5ca71ef2ea63167e2d47211634e
sooraj s <sooraj1998ayiroor@gmail.com>
2020-08-13 09:50:11 +0530

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
   ],
-  providers: [],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
+  entryComponents: [
+    LoginComponent
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/dishes.ts
@@ -0,0 +1,173 @@
+import { Dish } from './dish';
+
+export const DISHES: Dish[] = [
+    {
+        id: '0',
+        name: 'Uthappizza',
+        image: '/assets/images/uthappizza.png',
+        category: 'mains',
+        featured: true,
+        label: 'Hot',
+        price: '4.99',
+        // tslint:disable-next-line:max-line-length
+        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'sooraj s',
+                date: '2019-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'Akhil s',
+                date: '2019-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Rahul',
+                date: '2019-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'pranav ',
+                date: '2019-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: 'Akshay',
+                date: '2019-12-02T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        id: '1',
+        name: 'Zucchipakoda',
+        image: '/assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        featured: false,
+        label: '',
+        price: '1.99',
+        description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'sooraj s',
+                date: '2019-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'Ajay ',
+                date: '2019-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Adithya',
+                date: '2019-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'Amar',
+                date: '2019-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: 'Anoop',
+                date: '2019-12-02T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        id: '2',
+        name: 'Vadonut',
+        image: '/assets/images/vadonut.png',
+        category: 'appetizer',
+        featured: false,
+        label: 'New',
+        price: '1.99',
+        description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'Ajold',
+                date: '2019-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'Aswin',
+                date: '2019-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Ajay',
+                date: '2019-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'Akhil',
+                date: '2019-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: 'Meenu',
+                date: '2019-12-02T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        id: '3',
+        name: 'ElaiCheese Cake',
+        image: '/assets/images/elaicheesecake.png',
+        category: 'dessert',
+        featured: false,
+        label: '',
+        price: '2.99',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'Anoop',
+                date: '2019-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'shaji',
+                date: '2019-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Aswin',
+                date: '2019-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'Ajay',
+                date: '2019-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: 'sooraj s',
+                date: '2019-12-02T17:57:28.556094Z'
+            }
+        ]
+    }
+];
