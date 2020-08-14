7f25416895b5aadbb3c02ad614079f1e1f1f9646
sooraj s <sooraj1998ayiroor@gmail.com>
2020-08-14 15:00:58 +0530

    1 error (not loaded promotion)

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
-import {Promotion} from '../shared/promotion';
-import {PROMOTIONS} from '../shared/promotions';
+
 
 @Injectable()
 export class LeaderService {
@@ -10,14 +9,20 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedleader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
+    });
   }
 }
