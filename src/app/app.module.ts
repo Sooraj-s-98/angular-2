f143af62a14c81295c8ef181352f7bff4f673f5b
sooraj s <sooraj1998ayiroor@gmail.com>
2020-08-17 15:33:58 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,7 +38,7 @@ import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 
 import { baseURL } from './shared/baseurl';
-
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,7 +49,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
